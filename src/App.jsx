712a21dbd0d6a6def715e21feb90d1d5ca8b8952
BrennanHall0918 <mercuryfree247@gmail.com>
2026-07-16 16:32:55 -0500

    implimented react router; added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
