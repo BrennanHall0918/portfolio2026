6176eb33f9716442147e3aef99a7d559d3c37817
BrennanHall0918 <mercuryfree247@gmail.com>
2026-07-21 12:53:24 -0500

    added pages and routes to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+// Routes
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Contact from "./pages/Contact";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
+        <>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/experience" element={<Experience />} />
+                <Route path="/Contact" element={<Contact />} />
+            </Routes>
+        </>
     );
 }
 
